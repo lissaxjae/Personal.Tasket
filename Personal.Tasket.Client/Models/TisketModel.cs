ccc817cf443f1b76ee3d92986898bbf4dde5f739
Lissa Jae <lissaxjae@outlook.com>
2022-07-03 23:36:40 -0700

    Add project files.

---
+++ Personal.Tasket.Client/Models/TisketModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Personal.Tasket.Client.Models;
+
+public class TisketModel
+{
+    //public TisketModel(int id, string title, string description, DateTime whenAdded, DateTime? whenCompleted, bool isCompleted)
+    //{
+    //    Id = id;
+    //    Title = title ?? throw new ArgumentNullException(nameof(title));
+    //    Description = description ?? throw new ArgumentNullException(nameof(description));
+    //    WhenAdded = whenAdded;
+    //    WhenCompleted = whenCompleted;
+    //    IsCompleted = isCompleted;
+    //}
+
+    public int Id { get; set; }
+
+    [Required]
+    [DataType(DataType.Text), MaxLength(50)]
+    public string Title { get; set; } = string.Empty;
+
+    [DataType(DataType.Text), MaxLength(250)]
+    public string Description { get; set; } = string.Empty;
+
+    [DataType(DataType.DateTime)]
+    public DateTime WhenAdded { get; set; } = DateTime.Now.ToLocalTime();
+
+    [DataType(DataType.DateTime)]
+    public DateTime? WhenCompleted { get; set; } = null;
+    public bool IsCompleted { get; set; } = false;
+}
---
+++ Personal.Tasket.Client/TasketState.cs
@@ -0,0 +1,69 @@
+﻿using Personal.Tasket.Client.Models;
+
+public class TasketState
+{
+    public TisketModel Tisket { get; private set; } = new();
+    public static List<TisketModel> Tasket { get; private set; } = new();
+
+    private ILogger<TasketState> _logger;
+
+	public TasketState(ILogger<TasketState> logger)
+	{
+		_logger = logger;
+	}
+
+    public async Task AddFormTisket(TisketModel tisketForm)
+    {
+        Tisket = new()
+        {
+            Id = Tasket.Count == 0 ? 1: Tasket.Last().Id + 1,
+            Title = tisketForm.Title,
+            Description = tisketForm.Description
+        };
+        Tasket.Add(Tisket);
+        await Task.FromResult(Tasket.Exists(t => t.Id == Tisket.Id));
+        _logger.LogInformation($"Tisket {Tisket.Id}, {Tisket.Title}, added {Tisket.WhenAdded.ToString()}");
+    }
+
+    public async Task CompleteTisket(TisketModel completedTisket)
+    {
+        completedTisket.IsCompleted = true;
+        completedTisket.WhenCompleted = DateTime.Now.ToLocalTime();
+        var completed = Tasket.Find(t => t.Id == completedTisket.Id);
+        if (completed != null)
+        {
+            await Task.FromResult(completed = completedTisket);
+        }
+    }
+    
+    public async Task DeleteTisket(TisketModel selectedTisket)
+    {
+        await Task.FromResult(Tasket.Remove(selectedTisket));
+    }
+
+    public void ResetTisket()
+    {
+        Tisket = new();
+        
+    }
+
+    public void EmptyTasket()
+    {
+        Tasket.Clear();
+    }
+
+    public void LoadDummyData(int dataAmount)
+    {
+        for (int i = 0; i < dataAmount; i++)
+        {
+            var id = Tasket.Count == 0 ? 1 : Tasket.Last().Id + 1;
+
+            Tasket.Add(new() 
+            { 
+                Id = id, 
+                Title = $"Title {id}", 
+                Description = $"Description {id}" 
+            });
+        }
+    }
+}
